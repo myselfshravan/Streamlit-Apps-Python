af39c727405f67720796d8a0e3b792946f5e0afb
Shravan <shravanrevanna@gmail.com>
2022-11-25 02:15:26 +0530

    Update bankstreamlit.py

--- bankstreamlit.py
+++ bankstreamlit.py
@@ -13,7 +13,7 @@ st.title('Visualize Your Bank Statement')
 agree = st.checkbox('Use Demo Data')
 
 if agree:
-    uploaded_file = 'https://github.com/myselfshravan/Python/blob/main/statement23.xls'
+    uploaded_file = 'https://github.com/myselfshravan/Python/files/10087176/statement23.xls'
 else:
     uploaded_file = st.file_uploader("Choose a Excel file of HDFC Bank Statement", type="xls")
 
