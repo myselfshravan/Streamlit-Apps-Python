13f3ff799d1b01e039a9cdd1e29ed4a27ea0f1da
Shravan <shravanrevanna@gmail.com>
2022-11-25 02:40:23 +0530

    Update and rename exam.py to Scraping_Results_EvenSem.py

--- Scraping_Results_EvenSem.py
+++ Scraping_Results_EvenSem.py
@@ -2,30 +2,34 @@ from selenium import webdriver
 
 B_URL = "./chromedriver.exe"
 T_URL = "https://exam.msrit.edu/eresultseven/"
-DEPT = "CS"
+DEPT = "CY"
+TOLERANCE = 5
 browser = webdriver.Chrome(executable_path=B_URL)
-browser.implicitly_wait(20)
 browser.get(T_URL)
-cap = input("enter the captcha: ")
+cap = input("Enter the captcha: ")
 
 with open(f'#{DEPT}.csv', 'w+') as f:
     capt = browser.find_element_by_id("osolCatchaTxt0")
     capt.send_keys(cap)
     capt_click = browser.find_element_by_class_name("buttongo")
     element = browser.find_element_by_id("usn")
+    tol = TOLERANCE
     for i in range(1, 200):
         element.clear()
         element.send_keys(f"1MS21{DEPT}{i:03}")
         capt_click.click()
-        browser.implicitly_wait(20)
         try:
             name = browser.find_element_by_tag_name("h3")
             cgpa = browser.find_elements_by_tag_name("p")
-            data = f'1MS21{DEPT}{i:03}, {name.text}, {float(cgpa[3].text):.2f}\n'
+
+            data = f'1MS21{DEPT}{i:03}, {name.text}, {cgpa[3].text}\n'
             f.write(data)
-            print(f"{name.text}: {cgpa[3].text}")
+            print(f"{i + 1} {name.text}: {cgpa[3].text}")
+            tol = TOLERANCE
         except Exception as e:
             print(e)
-            break
+            tol -= 1
+            if tol == 0:
+                break
         browser.back()
     browser.close()
