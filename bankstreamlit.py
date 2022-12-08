a6a40844b2af90746ea6dfc563f09a00c5f7e799
Shravan <shravanrevanna@gmail.com>
2022-12-09 01:28:46 +0530

    Update bankstreamlit.py

--- bankstreamlit.py
+++ bankstreamlit.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
-from datetime import date, datetime, timedelta
+from datetime import date, datetime
 import plotly.express as px
 
-st.title('Visualize Your Bank Statement')
+st.title('Visualize Your HDFC Bank Statement')
+st.write('Export your HDFC Bank statement as a XLS file and drop it here to analyze your expenses')
+st.write("Note: We don't store your data. It's all done locally on your machine")
 
-agree = st.checkbox('Use Demo Data')
-
+agree = st.checkbox('Use Sample Data')
 if agree:
     uploaded_file = 'https://github.com/myselfshravan/Python/files/10087176/statement23.xls'
 else:
@@ -44,8 +45,14 @@ if uploaded_file is not None:
     end = datetime.strptime(df['Date'].iloc[-1].strftime('%d/%m/%y'), '%d/%m/%y')
     days = (end - start).days
     st.write(f"Number of Days: {days}")
+    total_withdrawal = df['Withdrawal'].sum()
+    st.write(f"Total Withdrawal: {total_withdrawal}")
+    total_deposit = df['Deposited'].sum()
+    st.write(f"Total Deposit: {total_deposit}")
 
-    fig = px.line(df, x='Date', y='Balance', title='Bank Balance')
+    fig = px.line(df, x='Date', y='Balance', title='Balance Trend', color_discrete_sequence=['#1f77b4'],
+                  template='plotly_white', labels={'Date': 'Date', 'Balance': 'Balance'},
+                  hover_data={'Date': False, 'Balance': ':.2f'})
     st.plotly_chart(fig, use_container_width=True)
 
     st.dataframe(df, use_container_width=True)
@@ -64,48 +71,56 @@ if uploaded_file is not None:
 
     date_selected = st.date_input('Select Date', value=date(2022, 11, 4))
     selected = df.loc[df['Date'] == date_selected]
+    st.dataframe(selected, use_container_width=True)
     st.write("Total Withdrawals on", date_selected, "is", selected['Withdrawal'].sum())
     st.write("Total Deposits on", date_selected, "is", selected['Deposited'].sum())
     st.write("\n")
     st.subheader('Select a date range')
-    start_date = st.date_input('Start date', datetime(2022, 11, 3))
-    end_date = st.date_input('End date', datetime(2022, 11, 17))
+    start_range = df['Date'].iloc[0]
+    end_range = df['Date'].iloc[-1]
+    start_date = st.date_input('Start date', value=start_range)
+    end_date = st.date_input('End date', value=end_range)
     mask = (df['Date'] >= start_date) & (df['Date'] <= end_date)
     df = df.loc[mask]
     st.write(df, use_container_width=True)
-    st.write('Total Deposited: ', df['Deposited'].sum())
-    st.write('Total Withdrawal: ', df['Withdrawal'].sum())
+    st.write(f'Total Deposited: Rs {df["Deposited"].sum()}')
+    st.write(f'Total Withdrawal: Rs {df["Withdrawal"].sum()}')
 
-    # st.subheader('Select What type of Graph you want to Visualize')
-    # graph = st.radio('Select', ('Line', 'Bar'))
+    st.subheader('Total amount spent on each UPI')
+    st.write(df.groupby('UPIs')['Withdrawal'].sum().sort_values(ascending=False), use_container_width=True)
 
-    # fig, ax = plt.subplots()
-    # ax.plot(df['Date'], df['Balance'], marker='o', linewidth=1, markersize=1)
-    # plt.xticks(rotation=90)
-    # plt.yticks(np.arange(0, 17000, 1000))
-    # ax.set(xlabel='Date', ylabel='Balance', title='Balance')
-    # ax.grid()
-    # st.pyplot(fig)
+    st.subheader('Highest amount spent in one transaction')
+    st.write(df.loc[df['Withdrawal'].idxmax()], use_container_width=True)
 
-    # st.write(df)
-    # st.write("This is a bar chart of the bank statement")
-    # fig = px.bar(df, x='Date', y='Amount', color='Type')
-    # st.plotly_chart(fig)
-    # st.write("This is a pie chart of the bank statement")
-    # fig = px.pie(df, values='Amount', names='Type')
-    # st.plotly_chart(fig)
-    # st.write("This is a line chart of the bank statement")
-    # fig = px.line(df, x='Date', y='Amount', color='Type')
-    # st.plotly_chart(fig)
-    # st.write("This is a histogram of the bank statement")
-    # fig = px.histogram(df, x='Amount', color='Type')
-    # st.plotly_chart(fig)
-    # st.write("This is a box plot of the bank statement")
-    # fig = px.box(df, x='Amount', color='Type')
-    # st.plotly_chart(fig)
-    # st.write("This is a violin plot of the bank statement")
-    # fig = px.violin(df, y='Amount', color='Type')
-    # st.plotly_chart(fig)
-    # st.write("This is a scatter plot of the bank statement")
-    # fig = px.scatter(df, x='Date', y='Amount', color='Type')
-    # st.plotly_chart(fig)
+    inday = df.groupby("Date")['Withdrawal'].sum().sort_values(ascending=False).head(1).index[0].strftime("%d %B")
+    st.subheader(f'Highest amount spent in a day')
+    amount = df.groupby("Date")['Withdrawal'].sum().sort_values(ascending=False).head(1).values[0]
+    st.write(f'{inday} : Rs {amount}')
+
+hide_streamlit_style = """
+                    <style>
+                    # MainMenu {visibility: hidden;}
+                    footer {visibility: hidden;}
+                    footer:after {
+                    content:'Made with ❤️ by Shravan'; 
+                    visibility: visible;
+    	            display: block;
+    	            position: relative;
+    	            # background-color: red;
+    	            padding: 15px;
+    	            top: 2px;
+    	            }
+                    </style>
+                    """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# st.subheader('Select What type of Graph you want to Visualize')
+# graph = st.radio('Select', ('Line', 'Bar'))
+
+# fig, ax = plt.subplots()
+# ax.plot(df['Date'], df['Balance'], marker='o', linewidth=1, markersize=1)
+# plt.xticks(rotation=90)
+# plt.yticks(np.arange(0, 17000, 1000))
+# ax.set(xlabel='Date', ylabel='Balance', title='Balance')
+# ax.grid()
+# st.pyplot(fig)
