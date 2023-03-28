df14e092aff0436190024f4761e07477a3c42288
Shravan <shravanrevanna@gmail.com>
2023-03-28 21:09:22 +0530

    Update bankstreamlit.py

--- bankstreamlit.py
+++ bankstreamlit.py
@@ -14,28 +14,24 @@ else:
     uploaded_file = st.file_uploader("Choose a Excel file of HDFC Bank Statement", type="xls")
 
 if uploaded_file is not None:
-
     df = pd.read_excel(uploaded_file)
-
     df = df.iloc[21:-18]
-
     df = df.drop(df.columns[[0, 2]], axis=1)
-
     df = df.drop(df.index[1])
-
     df = df.fillna(0)
     df.rename(
         columns={'Unnamed: 1': 'UPIs', 'Unnamed: 3': 'Date', 'Unnamed: 4': 'Withdrawal', 'Unnamed: 5': 'Deposited',
                  'Unnamed: 6': 'Balance'},
         inplace=True)
 
-    df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%y').dt.date  # Converting date to datetime format
+    df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%y').dt.date
     df['Withdrawal'] = df['Withdrawal'].apply(lambda x: "{:.1f}".format(x)).astype(float)
     df['Deposited'] = df['Deposited'].apply(lambda x: "{:.1f}".format(x)).astype(float)
     df['Balance'] = df['Balance'].astype(float)
     df['UPIs'] = df['UPIs'].astype(str)
     df['UPIs'] = df['UPIs'].str.split('@', expand=True)[0]
     df['UPIs'] = df['UPIs'].str.split('-', expand=True)[1]
+    df.index = range(1, len(df) + 1)
 
     start_date = df['Date'].iloc[0].strftime("%B %d")
     end_date = df['Date'].iloc[-1].strftime("%B %d")
@@ -49,31 +45,51 @@ if uploaded_file is not None:
     st.write(f"Total Withdrawal: {total_withdrawal}")
     total_deposit = df['Deposited'].sum()
     st.write(f"Total Deposit: {total_deposit}")
+    st.write(f"Closing and Opening Balance: {df['Balance'].iloc[0]} and {df['Balance'].iloc[-1]}")
+    time_frame = list(df['Date'])
+    withdrawal = list(df['Withdrawal'])
+    for i in range(1, len(withdrawal)):
+        withdrawal[i] = withdrawal[i] + withdrawal[i - 1]
+    deposited = list(df['Deposited'])
+    for i in range(1, len(deposited)):
+        deposited[i] = deposited[i] + deposited[i - 1]
 
-    fig = px.line(df, x='Date', y='Balance', title='Balance Trend', color_discrete_sequence=['#1f77b4'],
-                  template='plotly_white', labels={'Date': 'Date', 'Balance': 'Balance'},
-                  hover_data={'Date': False, 'Balance': ':.2f'})
-    st.plotly_chart(fig, use_container_width=True)
+    balance = list(df['Balance'])
+    line = pd.DataFrame({'Balance': balance}, index=time_frame)
+    st.subheader('Balance Trend')
+    st.line_chart(line, use_container_width=True)
 
+    # fig = px.line(df, x='Date', y='Balance', title='Balance Trend', color_discrete_sequence=['#1f77b4'],
+    #               template='plotly_white', labels={'Date': 'Date', 'Balance': 'Balance'},
+    #               hover_data={'Date': False, 'Balance': ':.2f'})
+    # st.plotly_chart(fig, use_container_width=True)
     st.dataframe(df, use_container_width=True)
 
     val = st.radio('Select', ('Withdrawal', 'Deposited'))
     if val == 'Withdrawal':
+        withdraw_line = pd.DataFrame({'Withdrawal': withdrawal}, index=time_frame)
+        st.subheader('Withdrawal Trend')
+        st.line_chart(withdraw_line, use_container_width=True)
         fig = px.bar(df, x='Date', y='Withdrawal', title='Withdrawals')
         st.plotly_chart(fig, use_container_width=True)
         figs = px.scatter(df, x='Date', y='Withdrawal', color='UPIs', title='Withdrawals')
         st.plotly_chart(figs, use_container_width=True)
     elif val == 'Deposited':
+        deposit_line = pd.DataFrame({'Deposited': deposited}, index=time_frame)
+        st.subheader('Deposit Trend')
+        st.line_chart(deposit_line, use_container_width=True)
         fig = px.bar(df, x='Date', y='Deposited', title='Deposits')
         st.plotly_chart(fig, use_container_width=True)
         figs = px.scatter(df, x='Date', y='Deposited', color='UPIs', title='Deposits')
         st.plotly_chart(figs, use_container_width=True)
 
-    date_selected = st.date_input('Select Date', value=date(2022, 11, 4))
+    first_date = df['Date'].iloc[0]
+
+    date_selected = st.date_input('Select Date', value=first_date)
     selected = df.loc[df['Date'] == date_selected]
     st.dataframe(selected, use_container_width=True)
-    st.write("Total Withdrawals on", date_selected, "is", selected['Withdrawal'].sum())
-    st.write("Total Deposits on", date_selected, "is", selected['Deposited'].sum())
+    st.write("Total Withdrawals on", date_selected.strftime("%d %B"), "is", selected['Withdrawal'].sum())
+    st.write("Total Deposits on", date_selected.strftime("%d %B"), "is", selected['Deposited'].sum())
     st.write("\n")
     st.subheader('Select a date range')
     start_range = df['Date'].iloc[0]
@@ -95,7 +111,7 @@ if uploaded_file is not None:
     inday = df.groupby("Date")['Withdrawal'].sum().sort_values(ascending=False).head(1).index[0].strftime("%d %B")
     st.subheader(f'Highest amount spent in a day')
     amount = df.groupby("Date")['Withdrawal'].sum().sort_values(ascending=False).head(1).values[0]
-    st.write(f'{inday} : Rs {amount}')
+    st.write(f'On {inday} : Rs {amount}')
 
 hide_streamlit_style = """
                     <style>
